bf01c16f7de95df969ebf335c3157a0114621744
viftergr <73195334+viftergr@users.noreply.github.com>
2022-04-05 05:53:37 +0530

    Update cybv.c

--- beginner(0 - 1000)/math/cybv.c
+++ beginner(0 - 1000)/math/cybv.c
@@ -1,22 +1,26 @@
 //The Begining Era Of Cyberverse
 
+
 #include <stdio.h>
 #include <math.h>
 
 
 int main()
 {
-    int t,n,k,i,temp;
+    int t,n,k,i,temp,arr[100000];
     scanf("%d",&t);
     for(i=0;i<t;i++){
         scanf("%d %d",&n,&k);
         temp=k/n;
         if(k - temp*n==0){
-            printf("%d",0);
+            arr[i]=temp;
         }
         else{
-            printf("%d",1);
+            arr[i]=temp;
         }
+    }
+    for(i=0;i<t;i++){
+        printf("%d\n",arr[i]);
     }
     return 0;
 }
